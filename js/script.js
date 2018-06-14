5e94dd1d5a7462cf293dc2472ef582f0694ab703
Toussaint Dimitry <dimitrytoussaint2@gmail.com>
2018-06-14 15:41:55 +0200

    begin the projet; mouv and direction was ok

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var x = 500;
+var y = 500;
+var duck = document.createElement("div");
+document.body.appendChild(duck)
+console.log(duck);
+
+duck.style.background = "red";
+duck.style.position = "absolute";
+duck.style.width = "20px";
+duck.style.height = "20px";
+duck.style.top = y + "px";
+duck.style.left = x + "px";
+// duck.style.transition = "1s";
+// duck.style.rotate = "null";
+
+// while (y > 0) {
+setInterval(move, 1000);
+
+function move() {
+    duck.style.top = y + "px";
+    y -= 20;
+    duck.style.left = x + "px";
+    newX = parseInt(Math.random() * 1200);
+    console.log(newX);
+    x = newX;
+    if (x < newX) {
+        for (var i = x; i < newX; i++) {
+            x += 20;
+        }
+    } else {
+        for (var i = x; x > newX; i++) {
+            x -= 20;
+        }
+    }
+    if (y < -20) {
+        duck.remove();
+    }
+    // newY = Math.round(Math.random())*0;
+}
+// }
